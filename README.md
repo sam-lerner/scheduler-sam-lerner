90e96900e28202d51f6bfa04496e7a7eca776797
Sam Lerner <sam.a.lerner@gmail.com>
2022-12-05 18:06:32 -0500

    README update, script cleanup

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# scheduler-sam-lerner
+# Workday scheduler app
+
+## Description
+
+This webpage hosts a scheduling program to allow the user to keep track of the day's events. The current timeblock is highlighted in red, with gray blocks indicating the past and green the future. 
+
+[Workday Scheduler](https://sam-lerner.github.io/scheduler-sam-lerner/ "Go to the Website!")
+
+## Mock-Up
+
+The following is a screen shot of how past events appear on the website:
+
+![website demo](./assets/images/screen-shot.jpg)
+
+
+## Installation
+
+N/A
+
+## Usage
+
+After accessing the website, enter your appointment into the center column of text boxes and click the blue save button. Your events will be saved for future viewing.
+
+## Credits
+
+Thank you to classmates Eric Kirberger and Azime Nail, tutor Dru Sanchez, TAs Paul Cwik and Manoli Koutouzos for their guidance with this project. 
+
+## License
+
+N/A
+
+## Badges
+
+![github](https://img.shields.io/github/followers/sam-lerner?style=social)
+
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,65 +1,12 @@
-// What are we doing here?
 
-// When I click into a timeblock, I can enter an event (text entry)
-// Already done!
-// When I click the save button for that timeblock, the text for the event is saved into localstorage
 // Storing vars
 var saveBtn = $('.saveBtn');
-// var eventText = $('.description');
-
+var timeText = $(".description");
 // Time sync
 var currentStorage = [];
 var currentDay;
 var currentTime;
 var timeList = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-var timeText = $(".description");
-
-// Save to localStorage.
-// function setLocalStorage(addedEvent) {
-//   var currentStorage = getLocalStorage();
-//   currentStorage.push(addedEvent);
-//   console.log('saving storage: ' + currentStorage);
-//   localStorage.setItem('event', JSON.stringify(currentStorage));
-// }
-
-
-// Read from localStorage!
-// function getLocalStorage() {
-//   var currentStorage = localStorage.getItem('event');
-//   if (currentStorage) {
-//     currentStorage = JSON.parse(currentStorage);
-//   } else {
-//     currentStorage = [];
-//   }
-//   console.log('localstorage: ' + currentStorage)
-//   return currentStorage;
-// }
-
-// Display saved info on page
-// function displayLocalStorage() {
-//   var currentStorage = getLocalStorage('event');
-//   for (var i = 0; i < currentStorage.length; i++) {
-//     var textToDisplay = currentStorage[i].text;
-//     timeText.value = textToDisplay;
-//   }}
-
-
-  // JSON.parse(localStorage.getItem('event'[0])) || [];
-  // for (let i = 0; i < timeList.length; i++) {
-  //   timeText.each(function (i, a) {
-  //     $(a).val(localStorage.getItem('text'[i]))
-  //   });
-  //   console.log("GetLocal" + timeText);
-  // }
-  //   $('timeText').each(function(i) {
-  //     eventText= (localStorage.getItem[i].value);
-  //     console.log("get " + eventText);
-  //   })
-  // }
-  // }
-
-  // Events persist on refresh
-
 
   // First, get time from DayJS
   function currentDayJs() {
@@ -71,15 +18,15 @@ var timeText = $(".description");
 
   // When I view the timeblocks for the day, each timeblock is color coded to indicate whether it is in the past, present, or future
   function setColor() {
-    console.log("Setting color");
+    // console.log("Setting color");
     // First, ref current time.
     timeText.each(function (i) {
       currentTime = parseInt(dayjs().format('H'));
       // Get number from ID field
       blockTime = $(this).attr('id').split('-')[1];
       // Next, compare each block to current time.
-      console.log(i, currentTime);
-      console.log(i, blockTime)
+      // console.log(i, currentTime);
+      // console.log(i, blockTime)
       // If it's later:
       if (currentTime < blockTime) {
         $(this).removeClass('past present');
@@ -97,7 +44,6 @@ var timeText = $(".description");
         console.log('You have become unstuck in time.');
       }
       setInterval(setColor, 60000);
-      // getLocalStorage();
     })
   }
 
@@ -128,25 +74,7 @@ var timeText = $(".description");
   saveBtn.each(function (i) {
     $(this).click(function (event) {
       // event.preventDefault();
-      console.log("here", timeText[i].getAttribute('id').split('-')[1])
-      console.log("clicked")
-      console.log(timeText[i].value);
       localStorage.setItem($(this).attr('id'),timeText[i].value);
-      console.log('Saved', timeText.val);
+      // console.log('Saved', timeText[i].value);
     })
   })
-
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page
-
-
-  // blockTimeViaCustomDataAttribute
